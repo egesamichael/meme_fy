4c5592839c5f5e243166fa3cd847f64b7c1d3f08
egesamichael <praisewebhosts@gmail.com>
2024-04-17 04:57:42 +0300

    Cleaning Default Files

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+function Header() {
+  return (
+    <div className='header'>MeMe Fy</div>
+  )
+}
+
+export default Header
